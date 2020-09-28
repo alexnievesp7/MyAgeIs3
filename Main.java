9a205a2ae5de78bbf64c6c2ae68ce43d1d099fe3
alexnievesp7 <alejandro.nieves@patelhs.org>
2020-09-28 00:35:28 +0000

    ran it and it says im 15 which is right

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    int myBirthday= 519;
+    int todaysDate= 921;
+    if (myBirthday <= todaysDate)
+      System.out.println("I am " + myAge + "years old");
+  else{ 
+    System.out.println("I am still" + myAge + "years old");
+    }
   }
 }
